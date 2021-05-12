86d86f753ee9ed4cc09fa834c4df1fd8840c6195
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-05-12 15:54:58 +0700

    add order and payment log

---
+++ app/Models/PaymentLog.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentLog extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = ['payment_type', 'order_id', 'raw_response', 'status'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+        'metadata' => 'array',
+    ];
+}
